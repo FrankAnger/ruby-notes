3e1d4285fb3386670407a914f72d990eadf37f07
FrankAnger <thefrankanger@gmail.com>
2025-09-10 22:35:51 +0300

    Note CRUD and User Authentication

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,47 @@
+class NotesController < ApplicationController
+  before_action :set_product, only: %i[ show edit update destroy ]
+  def index
+    @notes = Note.all
+  end
+
+  def show
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to @note, notice: "Note was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @note.update(note_params)
+      redirect_to @note, notice: "Note was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @note.destroy
+    redirect_to notes_path, notice: "Note was successfully destroyed."
+  end
+
+  private
+    def set_product
+      @note = Note.find(params[:id])
+    end
+
+    def note_params
+      params.expect(note: [ :name ])
+    end
+end
