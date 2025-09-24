678304c26077d02a4b68abf775001917d44c028b
FrankAnger <thefrankanger@gmail.com>
2025-09-24 21:11:44 +0300

    wip(notifs): finished adding subscribers.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :notes
+  resources :notes do
+    resources :subscribers, only: [ :create ]
+  end
 
   root "notes#index"
 
